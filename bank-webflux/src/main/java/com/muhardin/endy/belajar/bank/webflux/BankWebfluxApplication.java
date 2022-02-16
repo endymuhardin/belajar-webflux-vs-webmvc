22e4fa41d02e38180b2b84554022ecd7bbed7dfb
Endy Muhardin <endy.muhardin@gmail.com>
2022-02-16 20:45:18 +0700

    starter project webflux

---
+++ bank-webflux/src/main/java/com/muhardin/endy/belajar/bank/webflux/BankWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.bank.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWebfluxApplication.class, args);
+	}
+
+}
---
+++ bank-webflux/src/test/java/com/muhardin/endy/belajar/bank/webflux/BankWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.bank.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
