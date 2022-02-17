f1bd9bd2fea809c7f404123667d1a4bce896f5af
Endy Muhardin <endy.muhardin@gmail.com>
2022-02-17 17:28:05 +0700

    test menggunakan step verifikasi

--- bank-webflux/src/test/java/com/muhardin/endy/belajar/bank/webflux/service/BankServiceTests.java
+++ bank-webflux/src/test/java/com/muhardin/endy/belajar/bank/webflux/service/BankServiceTests.java
@@ -6,45 +6,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
-import org.springframework.r2dbc.core.DatabaseClient;
-import org.springframework.util.StringUtils;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.util.StreamUtils;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.util.stream.Stream;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 public class BankServiceTests {
     @Autowired private BankService bankService;
-
-    @Autowired private DatabaseClient databaseClient;
+    @Autowired private R2dbcEntityTemplate entityTemplate;
     @Value("classpath:/sql/reset-data.sql") private Resource resetDatabaseScript;
 
     @BeforeEach
     public void resetDatabase() throws Exception {
-        Stream<String> stream = new BufferedReader(new InputStreamReader(resetDatabaseScript.getInputStream())).lines();
-        stream.forEach(sql -> {
-            if(StringUtils.hasText(sql)) {
-                databaseClient.sql(sql).then().block();
-            }
-        });
+        String query = StreamUtils.copyToString(resetDatabaseScript.getInputStream(), StandardCharsets.UTF_8);
+        Mono<Void> mono = this.entityTemplate
+                .getDatabaseClient()
+                .sql(query)
+                .then();
+
+        StepVerifier.create(mono).verifyComplete();
     }
 
     @Test
     public void testTransferSukses() {
-        bankService.transfer("N-001", "N-002", new BigDecimal(25000)).block();
+        StepVerifier.create(
+            bankService.transfer("N-001", "N-002", new BigDecimal(25000))
+        ).verifyComplete();
     }
 
     @Test
     public void testTransferRekeningTidakAktif() {
-        // gagal rollback berikut lognya
-        bankService.transfer("N-001", "N-003", new BigDecimal(25000)).block();
+        // runningnumber rollback (sesuai)
+        // mutasi rollback (sesuai)
+        // log error tidak rollback (sesuai)
+        // update saldo rekening pertama tidak rollback (tidak sesuai)
+        StepVerifier.create(
+            bankService.transfer("N-001", "N-003", new BigDecimal(25000))
+        ).verifyError();
     }
 
     @Test
     public void testTransferSaldoKurang() {
-        // ini rollback berikut lognya
-        bankService.transfer("N-001", "N-002", new BigDecimal(25000000)).block();
+        // runningnumber rollback (sesuai)
+        // mutasi rollback (sesuai)
+        // log error rollback (tidak sesuai)
+        // update saldo rekening rollback (sesuai)
+        StepVerifier.create(
+                bankService.transfer("N-001", "N-002", new BigDecimal(25000000))
+        ).verifyError();
     }
 }
