39b3742fada2417b75beb6c8f5975ab4462a95d8
Endy Muhardin <endy.muhardin@gmail.com>
2022-03-01 11:22:39 +0700

    change table names & column

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,54 +1,54 @@
 create extension if not exists pgcrypto;
 
-drop table if exists mutasi;
-drop table if exists rekening;
+drop table if exists transaction_history;
+drop table if exists account;
 drop table if exists running_number;
 
-create table rekening (
+create table account (
     id varchar(36) DEFAULT gen_random_uuid(),
-    nomor varchar(50) not null,
-    nama varchar(100) not null,
-    saldo decimal(19,2) not null,
-    aktif boolean not null,
+    account_number varchar(50) not null,
+    name varchar(100) not null,
+    balance decimal(19,2) not null,
+    active boolean not null,
     primary key (id), 
-    unique(nomor)
+    unique(account_number)
 );
 
-create table mutasi (
+create table transaction_history (
     id varchar(36) DEFAULT gen_random_uuid(),
-    id_rekening varchar(36) not null,
-    jenis_transaksi varchar(100) not null,
-    keterangan varchar(255) not null,
-    nilai decimal(19,2) not null,
-    waktu_transaksi timestamp not null,
-    referensi varchar(100) not null,
+    id_account varchar(36) not null,
+    transaction_type varchar(100) not null,
+    remarks varchar(255) not null,
+    amount decimal(19,2) not null,
+    transaction_time timestamp not null,
+    reference varchar(100) not null,
     primary key (id),
-    foreign key (id_rekening) references rekening,
-    unique(referensi)
+    foreign key (id_account) references account,
+    unique(reference)
 );
 
 create table running_number (
     id varchar(36) DEFAULT gen_random_uuid(),
-    jenis_transaksi varchar(100) not null,
+    transaction_type varchar(100) not null,
     reset_period date not null,
-    angka_terakhir bigint not null,
+    last_number bigint not null,
     primary key (id)
 );
 
-create table log_transaksi (
+create table transaction_log (
     id varchar(36) DEFAULT gen_random_uuid(),   
-    jenis_transaksi varchar(100) not null,
-    status_aktivitas varchar(100) not null,
-    waktu_aktivitas timestamp not null,
-    keterangan varchar(255) not null,
+    transaction_type varchar(100) not null,
+    activity_status varchar(100) not null,
+    activity_time timestamp not null,
+    remarks varchar(255) not null,
     primary key (id)
 );
 
-insert into rekening (id, nomor, nama, saldo, aktif)
-values ('n001', 'N-001', 'Nasabah 001', 1000000, true);
+insert into account (id, account_number, name, balance, active)
+values ('n001', 'C-001', 'Customer 001', 1000000, true);
 
-insert into rekening (id, nomor, nama, saldo, aktif)
-values ('n002', 'N-002', 'Nasabah 002', 2000000, true);
+insert into account (id, account_number, name, balance, active)
+values ('n002', 'C-002', 'Customer 002', 2000000, true);
 
-insert into rekening (id, nomor, nama, saldo, aktif)
-values ('n003', 'N-003', 'Nasabah 003', 3000000, false);
+insert into account (id, account_number, name, balance, active)
+values ('n003', 'C-003', 'Customer 003', 3000000, false);
